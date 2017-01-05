36393ba28837139e50a393fbcbd1a5cdac911bc2
ksivonci <ksivonci@gmail.com>
2017-01-05 13:39:04 +0100

    Converted to class

--- craiglist.py
+++ craiglist.py
@@ -2,89 +2,102 @@ from bs4 import BeautifulSoup # For HTML parsing
 import urllib2 # Website connections
 import re # Regular expressions
 
-#
-#  ----------------
-#
 
-def getParsedPage(pageUrl):
-    try:
-        response = urllib2.urlopen(pageUrl) # Connect to the job posting
-        site = response.read() # Read the job posting
-    except urllib2.HTTPError, e:
-      logging.error('HTTPError = ' + str(e.code)+ '  ' + website)
-    except urllib2.URLError, e:
-      logging.error('URLError = ' + str(e.reason)+ '  ' + website)
-    except httplib.HTTPException, e:
-      logging.error('HTTPException' + str(e)+ '  ' + website)
-    except Exception:
-      import traceback
-      logging.error('generic exception: ' + traceback.format_exc())
-    except: 
-      print "ERROR"
-      return None  # Need this in case the website isn't there anymore or some other weird connection problem 
+class CraigList:
 
-    soup_obj = BeautifulSoup(site, 'lxml') # Get the html from the site
+  aLinkMap = {}   # map of city/url links: city --> url
+  aStateMap = {}  # map of city/state:  city --> state
+  aJobCategoryMap = {} # map of job category/job url entry: category --> url entry
+  #
+  #  ----------------
+  #
+  def __init__(self):
+    if len(self.aLinkMap) != 0:
+      return
+      
+    soup_obj = self.getParsedPage('https://www.craigslist.org/about/sites')
+    self.extractCitiesCraiglistUrl(soup_obj)
     
-    if len(soup_obj) == 0: # In case the default parser lxml doesn't work, try another one
-        soup_obj = BeautifulSoup(site, 'html5lib')
-
-    return soup_obj
-
-#
-#  ----------------
-#
-
-def insertIntoMap(item, aLinkMap, state, aStateMap):
-  allLinks = item.find_all('a')
-  for aLink in allLinks:
-    key = aLink.get_text()
-    value = aLink.get('href')
-    if key in aLinkMap:
-      print ('In map already ' + key + ' ' + aLinkMap[key] + ' ' + value)
-      continue
-    aLinkMap[key] = value
-    aStateMap[key] = state
-
-#
-#  ----------------
-#
-
-def extractCitiesCraiglistUrl(soup_obj, aLinkMap, aStateMap):
-  aList = soup_obj.find_all('h4')
-  for item in aList:
-    insertIntoMap(item.find_next_sibling(), aLinkMap, item.get_text(), aStateMap)
+  #
+  #  ----------------
+  #
+  def getParsedPage(self, pageUrl):
+      try:
+          response = urllib2.urlopen(pageUrl) # Connect to the job posting
+          site = response.read() # Read the job posting
+      except urllib2.HTTPError, e:
+        logging.error('HTTPError = ' + str(e.code)+ '  ' + website)
+      except urllib2.URLError, e:
+        logging.error('URLError = ' + str(e.reason)+ '  ' + website)
+      except httplib.HTTPException, e:
+        logging.error('HTTPException' + str(e)+ '  ' + website)
+      except Exception:
+        import traceback
+        logging.error('generic exception: ' + traceback.format_exc())
+      except: 
+        print "ERROR"
+        return None  # Need this in case the website isn't there anymore or some other weird connection problem 
   
-#
-#  ----------------
-#
-
-def craiglist(country, state=None, city=None, filters=None):
-  if filters == None :
-    print '''Filters are following:
-      jobtype \n
-      aaaa \n
-      bbb'''
-    return
+      soup_obj = BeautifulSoup(site, 'lxml') # Get the html from the site
+      
+      if len(soup_obj) == 0: # In case the default parser lxml doesn't work, try another one
+          soup_obj = BeautifulSoup(site, 'html5lib')
   
-  print 'OK'
+      return soup_obj
+  
+  #
+  #  ----------------
+  #
+  def insertIntoMap(self, item, state):
+    allLinks = item.find_all('a')
+    for aLink in allLinks:
+      key = aLink.get_text()
+      value = aLink.get('href')
+      if key in self.aLinkMap:
+        print ('In map already ' + key + ' ' + self.aLinkMap[key] + ' ' + value)
+        continue
+      self.aLinkMap[key] = value
+      self.aStateMap[key] = state
+  
+  #
+  #  ----------------
+  #
+  def extractCitiesCraiglistUrl(self, soup_obj):
+    aList = soup_obj.find_all('h4')
+    for item in aList:
+      self.insertIntoMap(item.find_next_sibling(), item.get_text())
+    
+  #
+  #  ----------------
+  #
+  def dumpMaps(self):
+    
+    for aKey in self.aLinkMap.keys():
+      print (aKey + ' --> ' + self.aLinkMap[aKey])
+    
+    for aKey in self.aStateMap.keys():
+      print (aKey + ' --> ' + self.aStateMap[aKey])
+  
+  #
+  #  ----------------
+  #
+  def craiglist(country, state=None, city=None, filters=None):
+    if filters == None :
+      print '''Filters are following:
+        jobtype \n
+        aaaa \n
+        bbb'''
+      return
+    
+    print 'OK'
+  
 
-#
-#  ----------------
-#
+  #
+  #  ----------------
+  #
 
-#craiglist(country='US', state='CA', city='San Francisco', filters=None)
-
-aLinkMap = {}
-aStateMap = {}
-
-soup_obj = getParsedPage(pageUrl='https://www.craigslist.org/about/sites')
-extractCitiesCraiglistUrl(soup_obj, aLinkMap, aStateMap)
-
-
-for aKey in aLinkMap.keys():
-  print (aKey + ' --> ' + aLinkMap[aKey])
-
-for aKey in aStateMap.keys():
-  print (aKey + ' --> ' + aStateMap[aKey])
-
+  #craiglist(country='US', state='CA', city='San Francisco', filters=None)
+test = CraigList()
+#test.dumpMaps()  
+print ('link map lenght ' + str(len(test.aLinkMap)))
 
